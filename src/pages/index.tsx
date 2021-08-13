82d1ba6ebcfacd488b7cf456ec8e93e9dcaa7fde
wesley <wes.academico@gmail.com>
2021-08-12 21:52:58 -0300

    add husky and lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 
-export default function Home() {
+type Props = {
+  title: string
+}
+
+const bla = 'bla'
+const bla = 'bla'
+export default function Home({ title = 'React Avançado' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +19,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">react</a>
         </h1>
 
         <p className={styles.description}>
@@ -23,7 +29,7 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
+            <h2>{title} &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
